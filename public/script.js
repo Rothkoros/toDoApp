bb6ffa9c81c6eec14f3968d04fb716ef32c326d7
Nicholas Dombrosky <ndombrosky@gmail.com>
2020-10-17 15:53:22 -0600

    added classes to be able to add a class toggle.

--- public/script.js
+++ public/script.js
@@ -3,13 +3,20 @@ const clear = document.querySelector(".clear");
 const dateElement = document.getElementById("date");
 const list = document.getElementById("list");
 const input = document.getElementById("input");
+const CHECK = "fa-check-circle";
+const UNCHECK = "fa-circle-thin";
+const LINE_Through = "lineThrough";
+let LIST = [];
+let id = 0;
 
-
-function addToDo(toDo) {
+function addToDo(toDo, id, done, trash) {
+    if (trash) { return; }
+    const DONE = done ? CHECK : UNCHECK;
+    const LINE = done ? LINE_Through : "";
     const text = `<li class="item">
-                    <i class="co fa fa-circle-thin" job="complete"></li>
-                    <p class="text"> ${toDo}</p>
-                    <i class="de fa fa-trash-o" job="delete"></li>
+                    <i class="fa ${DONE} complete "  job="complete" id="${id}"></li>
+                    <p class="text"> ${LINE}">${toDo}</p>
+                    <i class="de fa fa-trash-o" job="delete" id="${id}"></li>
                     </li>`
 
     const position = "beforeend";
@@ -22,7 +29,14 @@ document.addEventListener("keyup", function(event) {
         const toDo = input.value;
         if (toDo) {
             addTod(toDo, id, false, false);
+            LIST.push({
+                name: toDo,
+                id: id,
+                done: false,
+                trash: false
+            });
             input.value = "";
+            id++;
         }
     }
 
